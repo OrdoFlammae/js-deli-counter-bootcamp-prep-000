012854bbe17328baba75f04324dcdfb58b8da8da
Aiden Loyd <aidensaloyd@gmail.com>
2018-06-05 02:01:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function takeANumber(katzDeliLine, name) {
-  return katzDeliLine
+  return katzDeliLine.indexOf(name) + 1;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return katzDeliLine.shift();
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var string = "The line is currently: ";
+  
+  if (katzDeliLine.length == 0) {
+    return "The line is currently empty.";
+  }
+  
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    string += `${i}. ${katzDeliLine[i]}`;
+    
+    if (i != katzDeliLine.length - 1) {
+      string += ", ";
+    }
+  }
+  
+  return string;
 }
